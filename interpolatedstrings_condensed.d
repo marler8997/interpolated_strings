ec4ea06ea5b340e78d61279102f9d9c3658aa6ee
Jonathan Marler <johnnymarler@gmail.com>
2017-10-12 08:02:15 -0600

    Initial commit

---
+++ interpolatedstrings_condensed.d
@@ -0,0 +1,124 @@
+import std.stdio;
+import std.string : indexOf;
+
+template tuple(T...)
+{
+    alias tuple = T;
+}
+
+string interpolatedTupleMixin(string str) pure
+{
+    return "tuple!(" ~ interpolatedMixinHelper1(str) ~ ")";
+}
+private string interpolatedMixinHelper1(string str) pure
+{
+    auto dollarIndex = str.indexOf("$");
+    if(dollarIndex < 0)
+    {
+        return "\"" ~ str ~ "\"";
+    }
+    if(dollarIndex == 0)
+    {
+        return interpolatedMixinHelper2(str[1..$]);
+    }
+    return "\"" ~ str[0..dollarIndex] ~ "\", " ~ interpolatedMixinHelper2(str[dollarIndex + 1..$]);
+}
+// star of str is a dollar expression
+private string interpolatedMixinHelper2(string str) pure
+{
+    if(str[0] == '(')
+    {
+        auto endParensIndex = str.indexOf(')');
+        if(endParensIndex < 0)
+        {
+            assert(0, "interpolated string has mismatch parens");
+        }
+        if(endParensIndex + 1 == str.length)
+        {
+            return str[1..endParensIndex];
+        }
+        return str[1..endParensIndex] ~ ", " ~ interpolatedMixinHelper1(str[endParensIndex + 1 .. $]);
+    }
+    assert(0, "interpolated expression not surrounded by '()' is not supported yet (expression is \"" ~ str ~ "\")");
+}
+
+void writeInterpolatedString(T...)(T interpolatedString)
+{
+    foreach(part; interpolatedString)
+    {
+        write(part);
+    }
+}
+auto formatter(T...)(T interpolatedString)
+{
+    struct Formatter
+    {
+        T interpolatedString;
+        void toString(scope void delegate(const(char)[]) sink) const
+        {
+            foreach(part; interpolatedString)
+            {
+                static if(__traits(compiles, sink(part)))
+                {
+                    sink(part);
+                }
+                else
+                {
+                    import std.format : formattedWrite;
+                    formattedWrite(sink, "%s", part);
+                }
+            }
+        }
+    }
+    return Formatter(interpolatedString);
+}
+
+
+int main(string[] args)
+{
+    foreach(item; i"hello\n")
+    {
+        write(item);
+    }
+    int a = 42;
+    foreach(item; i"$(a)\n")
+    {
+        write(item);
+    }
+    foreach(item; i"a is $(a)\n")
+    {
+        write(item);
+    }
+    foreach(item; i"1 + 2 is $(1 + 2)\n")
+    {
+        write(item);
+    }
+
+    writeInterpolatedString(i"hello\n");
+    writeInterpolatedString(i"$(a)\n");
+    writeInterpolatedString(i"a is $(a)\n");
+    writeInterpolatedString(i"1 + 2 is $(1 + 2)\n");
+
+    //
+    // Example of code generation
+    //
+    {
+        string functionName = "foo";
+        string operation = "+";
+        writeInterpolatedString(iq{
+            int $(functionName)(int x, int y)
+            {
+                return x $(operation) y;
+            }
+        }));
+    }
+    
+    // Example of passing an interpolated string to writefln
+    writefln("here is an interpolated string \"%s\"",
+        formatter(i"hello")));
+    writefln("here is an interpolated string \"%s\"",
+        formatter(i"1 + 2 is $(1 + 2)")));
+    
+
+    return 0;
+}
